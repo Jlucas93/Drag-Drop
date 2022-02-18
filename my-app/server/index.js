994effb1f5a34bbaa7aa07d631c468818528de49
João Lucas Nascimento Andrade <66873889+Jlucas93@users.noreply.github.com>
2022-02-18 15:18:54 -0300

    Add files via upload

---
+++ my-app/server/index.js
@@ -0,0 +1,31 @@
+const http = require('http')
+const fs = require('fs');
+
+/**
+ * @type {import('http').RequestListener}
+ */
+async function requestListener(request, response) {
+  const buffers = []
+  for await (const chunk of request) {
+    buffers.push(chunk)
+  }
+
+  const url = new URL(request.url, `http://${request.headers.host}`);
+  
+  const filename = url.searchParams.get('filename')
+  
+  
+  const data = Buffer.from(buffers)
+  
+
+  fs.writeFileSync(filename, data)
+
+  response.end();
+}
+
+http
+  .createServer(requestListener)
+  .listen(3001, () => {
+    console.log('Server started')
+  }
+  )
